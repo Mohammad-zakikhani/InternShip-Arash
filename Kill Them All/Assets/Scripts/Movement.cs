479731807d5b21c59b1f3d07ad5ebfbaffb4536b
ArashNoi <arashrajabi92@gmail.com>
2023-03-14 15:54:22 +0330

    Player Movement
    Added player movement scripts.

---
+++ Kill Them All/Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    private Rigidbody rb;
+    public Vector3 jump;
+    public float jumpForce = 4.0f;
+    public bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    private void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Physics.gravity * rb.mass);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
